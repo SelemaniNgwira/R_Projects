fb15caef624fc1c9f56da606b28db403e214964a
SelemaniNgwira <161382562+SelemaniNgwira@users.noreply.github.com>
2025-02-07 10:12:56 +0200

    This Pamodzi COVID-19 Project
    I have added Pamodzi COVID-19 Project

---
+++ Pamodzi Covid-19 Study/Sampling for District CBOs.R
@@ -0,0 +1,43 @@
+
+
+# Blantyre 
+## the are 8 best performing CBOs in Blantyre we only randomly select one
+## using the sample function below. this code is run once 
+
+sample(8, 1, replace = F) # Number 8 was picked
+
+
+# Mzimba North
+## the are 13 best and better  performing  CBOs in Mzimba North we only randomly select one
+## using the sample function below. this code is run once
+
+sample(13, 1, replace = F) # Number 8 was picked
+
+
+# Likoma 
+## the are 8 best performing CBOs in Likoma we only randomly select one
+## using the sample function below. this code is run once 
+
+sample(8, 1, replace = F) # Number 8 was picked
+
+
+
+# Lilongwe
+## the are 8 best and better  performing  CBOs in Lilongwe we only randomly select one
+## using the sample function below. this code is run once
+
+sample(9, 1, replace = F) # Number 2 was picked
+
+
+# Ntchisi
+## the are 3 best and better  performing  CBOs in Ntchisi we only randomly select one
+## using the sample function below. this code is run once
+
+sample(3, 1, replace = F) # Number  was picked
+
+
+# Mwanza 
+## the are 7 best and better  performing  CBOs in Mwanza with Whom Contact was avaiable we only randomly select one
+## using the sample function below. this code is run once
+
+sample(7, 1, replace = T) # Number 7 was picked
