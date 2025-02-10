8194b6764cd5bda0e932388ed111e435b437b660
SelemaniNgwira <161382562+SelemaniNgwira@users.noreply.github.com>
2025-02-10 13:24:01 +0200

    update code of the Pamodzi Study
    I have include a find function in within the script

--- Pamodzi Covid-19 Study/R script/helpfun.R
+++ Pamodzi Covid-19 Study/R script/helpfun.R
@@ -1,28 +1,40 @@
 
 
 # this function will run t.tset or Wilcoxon test based on shapiro test p-value
-my_fun <- function(df_paired) {
+my_fun <- function(data, x, y) {
   
-  # Check for normality using Shapiro-Wilk test on the differences
-  diff_scores <- df_paired$moh_score - df_paired$gen_score
-  shapiro_test <- shapiro.test(diff_scores)
-  
-  if (shapiro_test$p.value > 0.05) {
-    
-    # If normal, run paired t-test
-    t_test_result <- t.test(df_paired$moh_score, df_paired$gen_score, paired = TRUE)
-    return(t_test_result)
-    
-  } else {
-    
-    # If not normal, run Wilcoxon signed-rank test
-    df_filtered <- df_paired[df_paired$moh_score != df_paired$gen_score, ]
-    wilcoxon_result <- wilcox.test(df_filtered$moh_score, df_filtered$gen_score, 
-                                   paired = TRUE, conf.int = TRUE, exact = FALSE)
-    return(wilcoxon_result)
-  }
+          # Extract the paired values
+          x_values <- data[[x]]
+          y_values <- data[[y]]
+          
+          # Check for normality using Shapiro-Wilk test on the differences
+          diff_scores <- x_values - y_values
+          shapiro_test <- shapiro.test(diff_scores)
+          
+          if (shapiro_test$p.value > 0.05) {
+            
+            # If normal, run paired t-test
+            t_test_result <- t.test(x_values, y_values, paired = TRUE)
+            return(t_test_result)
+            
+          } else {
+            
+            # If not normal, remove zero-difference pairs
+            non_zero_indices <- x_values != y_values
+            x_filtered <- x_values[non_zero_indices]
+            y_filtered <- y_values[non_zero_indices]
+            
+            # Run Wilcoxon signed-rank test
+            wilcoxon_result <- wilcox.test(x_filtered, y_filtered, 
+                                           paired = TRUE,
+                                           conf.int = TRUE,
+                                           exact = FALSE)
+            return(wilcoxon_result)
+          }
 }
 
        
-        
+# how to use the function
+
+# my_fun(data = , "x", "y")
  
