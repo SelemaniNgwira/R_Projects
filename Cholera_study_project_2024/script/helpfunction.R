cc523d680fc407a1ca976dbe91347e49589e9484
SelemaniNgwira <ngwisele@gmail.com>
2025-02-14 23:08:21 +0200

    update version of the code

--- Cholera_study_project_2024/script/helpfunction.R
+++ Cholera_study_project_2024/script/helpfunction.R
@@ -12,3 +12,26 @@ process_data <- function(data, group_var) {
     rowwise() %>%  # Apply row-wise operation
     mutate(total = sum(across(where(is.numeric)), na.rm = TRUE))  # Compute total
 }
+
+
+
+
+# Generic function to create a summary table
+create_summary_table <- function(data, group_var, response_var, adorn_totals = FALSE) {
+  table <- data %>%
+    group_by(.data[[group_var]]) %>%
+    count(.data[[response_var]]) %>%
+    drop_na(.data[[response_var]]) %>%
+    pivot_wider(names_from = .data[[response_var]], values_from = n, values_fill = list(n = 0)) %>%
+    mutate(total = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>%
+    mutate(across(-total, ~ round(. / total * 100, 2), .names = "{.col}_percent")) %>%  # Convert only counts to percentages
+    rename(Background_characteristic = !!group_var) %>%
+    select(Background_characteristic, ends_with("_percent"), total)  # Exclude total_percent
+  
+  # Apply adorn_totals if TRUE
+  if (adorn_totals) {
+    table <- table %>% adorn_totals("row")
+  }
+  
+  return(table)
+}
